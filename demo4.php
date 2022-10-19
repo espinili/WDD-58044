7d358c24bb371913bc528fbeaaebf9df76970c9f
espinili <90013858+espinili@users.noreply.github.com>
2022-10-19 16:01:56 +0800

    Add files via upload

---
+++ demo4.php
@@ -0,0 +1,45 @@
+<html>
+<body>
+<?php
+     
+     //$fuel = 10;
+	  //while($fuel>1)
+	 // {
+		  //keep driving
+		//  echo "DRIVE!!";
+	 // }
+	  
+	  //do while loop
+	 // $count = 1;
+	 // do
+	    //  echo"$count times 12 is" . $count*12 . "<br>";
+	//  while(++$COUNT <= 12);
+	  
+	  //for loop
+	 // for ($count = 1; $count <= 12; ++$count)
+	  //    echo"$count times 12 is" . $count*12 . "<br>";
+	 
+	  //for loop
+	  //for ($count = 1; $count <= 11; ++$count)
+	    //  echo"$Hello" . $count*11 . "<br>";
+	
+	//array
+	//$colors = array ("red", "blue", "green", "yellow");
+	//foreach ($colors as $value)
+	//{
+	//	echo"$value <br>";
+	//}
+	
+	//break
+	for ($x = 0; $x < 11; $x++)
+	{
+		if ($x == 11)
+		{
+			break;
+		}
+		echo"Hello: $x <br>";
+	}
+		
+//?>
+</body>
+</html>
